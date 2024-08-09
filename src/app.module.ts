c19eed8e3bd19965636e11374580e78d426578a1
Ralfarios <muamaralfarabi@gmail.com>
2024-08-09 15:10:45 +0700

    feat: plug subcategory module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ColorModule } from './modules/color/color.module';
 import { SizeModule } from './modules/size/size.module';
 import { CategoryModule } from './modules/category/category.module';
+import { SubcategoryModule } from './modules/subcategory/subcategory.module';
 
 @Module({
-  imports: [ColorModule, SizeModule, CategoryModule],
+  imports: [ColorModule, SizeModule, CategoryModule, SubcategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
